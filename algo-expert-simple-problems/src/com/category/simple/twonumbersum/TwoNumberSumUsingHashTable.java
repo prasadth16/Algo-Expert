3393f5ae268c193865896c606884842c61f15ac0
prasadth16 <dattaprasad.s.thakur@gmail.com>
2019-04-13 13:31:44 -0400

    solved the problem

--- algo-expert-simple-problems/src/com/category/simple/twonumbersum/TwoNumberSumUsingHashTable.java
+++ algo-expert-simple-problems/src/com/category/simple/twonumbersum/TwoNumberSumUsingHashTable.java
@@ -3,10 +3,15 @@ package com.category.simple.twonumbersum;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Time Complexity: O(n) where n=number of elements in the input array. As we
+ * are iterating through the array at least once space Complexity: O(n) where
+ * n=number of elements in the input array. As we are creating Hash Table
+ **/
 public class TwoNumberSumUsingHashTable {
 
 	public static void main(String[] args) {
-		int[] targetArray = getTwoNumberSum(new int[] { 7, -10, 6, 11, -1, 45 }, 10);
+		int[] targetArray = getTwoNumberSum(new int[] { -7, -5, -3, -1, 0, 1, 3, 5, 7 }, -5);
 		if (targetArray.length > 0) {
 			System.out.println("Target sum does match!!!");
 			for (int i : targetArray) {
@@ -19,19 +24,21 @@ public class TwoNumberSumUsingHashTable {
 
 	public static int[] getTwoNumberSum(int[] intArray, int targetSum) {
 		Map<Integer, Boolean> targetHash = new HashMap<>();
-		int currentNumber = 0;
-		int anotherNumber = 0;
-		int[] targetArray = new int[2];
+		Integer currentNumber = null;
+		Integer anotherNumber = null;
+		int[] targetArray;
 		for (int i : intArray) {
-			if (!targetHash.containsKey(targetSum - i)) {
+
+			if (!targetHash.containsKey(targetSum - (i))) {
 				targetHash.put(i, true);
 			} else {
 				currentNumber = i;
-				anotherNumber = targetSum - i;
+				anotherNumber = (targetSum - (i));
 				break;
 			}
 		}
-		if (currentNumber != 0 && anotherNumber != 0) {
+		if (currentNumber != null && anotherNumber != null) {
+			targetArray = new int[2];
 			if (currentNumber > anotherNumber) {
 				targetArray[0] = anotherNumber;
 				targetArray[1] = currentNumber;
@@ -39,6 +46,8 @@ public class TwoNumberSumUsingHashTable {
 				targetArray[0] = currentNumber;
 				targetArray[1] = anotherNumber;
 			}
+		} else {
+			targetArray = new int[] {};
 		}
 		return targetArray;
 	}
