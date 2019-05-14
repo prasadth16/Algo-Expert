a529a2d23e7dcfc085b03136e037f31284a37a3b
prasadth16 <dattaprasad.s.thakur@gmail.com>
2019-05-13 21:50:06 -0400

    committed Binary searched tree prgram

--- algo-expert-simple-problems/src/com/category/simple/datastructures/tree/BinarySearchTreeConstruction.java
+++ algo-expert-simple-problems/src/com/category/simple/datastructures/tree/BinarySearchTreeConstruction.java
@@ -3,6 +3,9 @@ package com.category.simple.datastructures.tree;
 /**
  * Binary Search Tree, Construction and Search Time Complexity for both the
  * operations: O(log n) Space Complexity for both the operations: O(log n)
+ * 
+ * Binary Search Tree Node deletion implemented iterativly and hence the time complexity is O(log n)
+ * and the space complexity is 1.
  **/
 public class BinarySearchTreeConstruction {
 
@@ -82,6 +85,66 @@ public class BinarySearchTreeConstruction {
 			searchBST(element, node.getRightReference());
 	}
 
+	public static Integer getLeftmostfromRightSubTree(TreeNode node) {
+		TreeNode currentNode = node;
+		TreeNode parentNode = node;
+		while (currentNode.getLeftReference() != null) {
+			parentNode = currentNode;
+			currentNode = currentNode.getLeftReference();
+		}
+		parentNode.setLeftReference(null);
+		return currentNode.getNodeData();
+	}
+
+	public static TreeNode deleteElementFromBST(TreeNode node, Integer element) {
+		boolean isOperationComplete = false;
+		boolean isLeft = false;
+		TreeNode currentNode = node;
+		TreeNode parentNode = node;
+		while (currentNode != null && !isOperationComplete) {
+			if (currentNode.getNodeData() == element) {
+
+				if (currentNode.getLeftReference() == null && currentNode.getRightReference() == null) {
+					if (isLeft) {
+						parentNode.setLeftReference(null);
+					} else {
+						parentNode.setRightReference(null);
+					}
+				} else if (currentNode.getLeftReference() != null && currentNode.getRightReference() != null) {
+					currentNode.setNodeData(getLeftmostfromRightSubTree(currentNode.getRightReference()));
+				} else {
+					if (currentNode.getLeftReference() != null) {
+						if (isLeft)
+							parentNode.setLeftReference(currentNode.getLeftReference());
+						else
+							parentNode.setRightReference(currentNode.getLeftReference());
+					} else {
+						if (isLeft) {
+							parentNode.setLeftReference(currentNode.getRightReference());
+						} else {
+							parentNode.setLeftReference(currentNode.getRightReference());
+						}
+
+					}
+				}
+				isOperationComplete = true;
+			}
+
+			if (currentNode.getNodeData() >= element) {
+				parentNode = currentNode;
+				currentNode = currentNode.getLeftReference();
+				isLeft = true;
+
+			} else {
+				parentNode = currentNode;
+				isLeft = false;
+				currentNode = currentNode.getRightReference();
+			}
+		}
+		return node;
+
+	}
+
 	public static void main(String[] arg) {
 		System.out.println("Constructing and searching element in BST==>");
 		Integer[] inputIntArray = new Integer[] { 17, 1, 45, 78, 43, 23, 15, 56, 76, -12, 100, 0, 22 };
@@ -95,6 +158,22 @@ public class BinarySearchTreeConstruction {
 		searchBST(0, rootNode);
 		searchBST(89, rootNode);
 		searchBST(-12, rootNode);
+
+		System.out.println("Deleting elments from BST==>");
+		Integer[] inputArray = new Integer[] { 23, 21, 27, 12, 22, 25, 30, 14, 24, 28, 42 };
+		rootNode = null;
+		for (Integer ipElement : inputArray) {
+			rootNode = addElementInBST(ipElement, rootNode);
+		}
+		System.out.println("BST ready for deletion!!!!");
+		// rootNode = deleteElementFromBST(rootNode, 14);
+		// System.out.println("Elment 14 deleted!!!!");
+		// rootNode = deleteElementFromBST(rootNode, 22);
+		// System.out.println("Elment 22 deleted!!!!");
+		rootNode = deleteElementFromBST(rootNode, 12);
+		System.out.println("Elment 12 deleted!!!!");
+		rootNode = deleteElementFromBST(rootNode, 27);
+		System.out.println("Elment 27 deleted!!!!");
 	}
 
 }
